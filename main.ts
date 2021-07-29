010f114de76004ff4ed36e5b3d745af74fd1bd24
helllomods <86760207+helllomods@users.noreply.github.com>
2021-07-29 12:04:00 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        edubitMotors.setServoPosition(ServoChannel.S1, 30)
+    } else if (input.buttonIsPressed(Button.B)) {
+        edubitMotors.setServoPosition(ServoChannel.S1, 150)
+    } else {
+        edubitMotors.setServoPosition(ServoChannel.S1, 90)
+    }
 })
